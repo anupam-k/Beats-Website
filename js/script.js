6755cffd25b5eaf61046bac69f03f10b862fff6d
Anupam Kumar Krishnan <91872149+anupam-k@users.noreply.github.com>
2023-03-07 21:56:17 +0530

    Added Files and Assets ✅

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Header On Scroll Color Change
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+// Menu
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+// Remove Menu On Click Any Menu Link
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
